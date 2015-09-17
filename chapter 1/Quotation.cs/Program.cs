233ed08750a828631d706410efd28fc012f47b4b
WillC336 <wclayton1166@gmail.com>
2015-09-17 11:31:44 -0400

    Commit 3
    finished quotations

---
+++ chapter 1/Quotation.cs/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Quotation.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("An eye for eye only ends up making the whole world blind.-M.K. Gandhi");
+        }
+    }
+}
