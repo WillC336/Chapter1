2b4e93906621da89caf454464f4440808bd4cf66
WillC336 <wclayton1166@gmail.com>
2015-09-17 11:28:37 -0400

    Commit 2
    finished address

---
+++ chapter 1/Address.cs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Address.cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Easton Area High School");
+            Console.WriteLine("2601 William Penn Highway");
+            Console.WriteLine("Easton,PA");
+        }
+    }
+}
